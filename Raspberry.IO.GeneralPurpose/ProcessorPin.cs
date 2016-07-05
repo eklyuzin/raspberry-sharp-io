7aceafcf917f4615df8e218052c19ce5a6994601
eklyuzin <eklyuzin@yandex.ru>
2016-07-05 23:26:19 +0300

    ProcessorPin: Add Pin33 - Pin128 enums

--- Raspberry.IO.GeneralPurpose/ProcessorPin.cs
+++ Raspberry.IO.GeneralPurpose/ProcessorPin.cs
@@ -218,6 +218,492 @@ namespace Raspberry.IO.GeneralPurpose
         /// <summary>
         /// Pin 32. (added for CubieTruck/CubieBoard3 compatibility
         /// </summary>
-        Pin32 = 32
+        Pin32 = 32,
+
+        /// <summary>
+        /// Pin 33.
+        /// </summary>
+        Pin33 = 33,
+
+        /// <summary>
+        /// Pin 34.
+        /// </summary>
+        Pin34 = 34,
+
+        /// <summary>
+        /// Pin 35.
+        /// </summary>
+        Pin35 = 35,
+
+        /// <summary>
+        /// Pin 36.
+        /// </summary>
+        Pin36 = 36,
+
+        /// <summary>
+        /// Pin 37.
+        /// </summary>
+        Pin37 = 37,
+
+        /// <summary>
+        /// Pin 38.
+        /// </summary>
+        Pin38 = 38,
+
+        /// <summary>
+        /// Pin 39.
+        /// </summary>
+        Pin39 = 39,
+
+        /// <summary>
+        /// Pin 40.
+        /// </summary>
+        Pin40 = 40,
+
+        /// <summary>
+        /// Pin 41.
+        /// </summary>
+        Pin41 = 41,
+
+        /// <summary>
+        /// Pin 42.
+        /// </summary>
+        Pin42 = 42,
+
+        /// <summary>
+        /// Pin 43.
+        /// </summary>
+        Pin43 = 43,
+
+        /// <summary>
+        /// Pin 44.
+        /// </summary>
+        Pin44 = 44,
+
+        /// <summary>
+        /// Pin 45.
+        /// </summary>
+        Pin45 = 45,
+
+        /// <summary>
+        /// Pin 46.
+        /// </summary>
+        Pin46 = 46,
+
+        /// <summary>
+        /// Pin 47.
+        /// </summary>
+        Pin47 = 47,
+
+        /// <summary>
+        /// Pin 48.
+        /// </summary>
+        Pin48 = 48,
+
+        /// <summary>
+        /// Pin 49.
+        /// </summary>
+        Pin49 = 49,
+
+        /// <summary>
+        /// Pin 50.
+        /// </summary>
+        Pin50 = 50,
+
+        /// <summary>
+        /// Pin 51.
+        /// </summary>
+        Pin51 = 51,
+
+        /// <summary>
+        /// Pin 52.
+        /// </summary>
+        Pin52 = 52,
+
+        /// <summary>
+        /// Pin 53.
+        /// </summary>
+        Pin53 = 53,
+
+        /// <summary>
+        /// Pin 54.
+        /// </summary>
+        Pin54 = 54,
+
+        /// <summary>
+        /// Pin 55.
+        /// </summary>
+        Pin55 = 55,
+
+        /// <summary>
+        /// Pin 56.
+        /// </summary>
+        Pin56 = 56,
+
+        /// <summary>
+        /// Pin 57.
+        /// </summary>
+        Pin57 = 57,
+
+        /// <summary>
+        /// Pin 58.
+        /// </summary>
+        Pin58 = 58,
+
+        /// <summary>
+        /// Pin 59.
+        /// </summary>
+        Pin59 = 59,
+
+        /// <summary>
+        /// Pin 60.
+        /// </summary>
+        Pin60 = 60,
+
+        /// <summary>
+        /// Pin 61.
+        /// </summary>
+        Pin61 = 61,
+
+        /// <summary>
+        /// Pin 62.
+        /// </summary>
+        Pin62 = 62,
+
+        /// <summary>
+        /// Pin 63.
+        /// </summary>
+        Pin63 = 63,
+
+        /// <summary>
+        /// Pin 64.
+        /// </summary>
+        Pin64 = 64,
+
+        /// <summary>
+        /// Pin 65.
+        /// </summary>
+        Pin65 = 65,
+
+        /// <summary>
+        /// Pin 66.
+        /// </summary>
+        Pin66 = 66,
+
+        /// <summary>
+        /// Pin 67.
+        /// </summary>
+        Pin67 = 67,
+
+        /// <summary>
+        /// Pin 68.
+        /// </summary>
+        Pin68 = 68,
+
+        /// <summary>
+        /// Pin 69.
+        /// </summary>
+        Pin69 = 69,
+
+        /// <summary>
+        /// Pin 70.
+        /// </summary>
+        Pin70 = 70,
+
+        /// <summary>
+        /// Pin 71.
+        /// </summary>
+        Pin71 = 71,
+
+        /// <summary>
+        /// Pin 72.
+        /// </summary>
+        Pin72 = 72,
+
+        /// <summary>
+        /// Pin 73.
+        /// </summary>
+        Pin73 = 73,
+
+        /// <summary>
+        /// Pin 74.
+        /// </summary>
+        Pin74 = 74,
+
+        /// <summary>
+        /// Pin 75.
+        /// </summary>
+        Pin75 = 75,
+
+        /// <summary>
+        /// Pin 76.
+        /// </summary>
+        Pin76 = 76,
+
+        /// <summary>
+        /// Pin 77.
+        /// </summary>
+        Pin77 = 77,
+
+        /// <summary>
+        /// Pin 78.
+        /// </summary>
+        Pin78 = 78,
+
+        /// <summary>
+        /// Pin 79.
+        /// </summary>
+        Pin79 = 79,
+
+        /// <summary>
+        /// Pin 80.
+        /// </summary>
+        Pin80 = 80,
+
+        /// <summary>
+        /// Pin 81.
+        /// </summary>
+        Pin81 = 81,
+
+        /// <summary>
+        /// Pin 82.
+        /// </summary>
+        Pin82 = 82,
+
+        /// <summary>
+        /// Pin 83.
+        /// </summary>
+        Pin83 = 83,
+
+        /// <summary>
+        /// Pin 84.
+        /// </summary>
+        Pin84 = 84,
+
+        /// <summary>
+        /// Pin 85.
+        /// </summary>
+        Pin85 = 85,
+
+        /// <summary>
+        /// Pin 86.
+        /// </summary>
+        Pin86 = 86,
+
+        /// <summary>
+        /// Pin 87.
+        /// </summary>
+        Pin87 = 87,
+
+        /// <summary>
+        /// Pin 88.
+        /// </summary>
+        Pin88 = 88,
+
+        /// <summary>
+        /// Pin 89.
+        /// </summary>
+        Pin89 = 89,
+
+        /// <summary>
+        /// Pin 90.
+        /// </summary>
+        Pin90 = 90,
+
+        /// <summary>
+        /// Pin 91.
+        /// </summary>
+        Pin91 = 91,
+
+        /// <summary>
+        /// Pin 92.
+        /// </summary>
+        Pin92 = 92,
+
+        /// <summary>
+        /// Pin 93.
+        /// </summary>
+        Pin93 = 93,
+
+        /// <summary>
+        /// Pin 94.
+        /// </summary>
+        Pin94 = 94,
+
+        /// <summary>
+        /// Pin 95.
+        /// </summary>
+        Pin95 = 95,
+
+        /// <summary>
+        /// Pin 96.
+        /// </summary>
+        Pin96 = 96,
+
+        /// <summary>
+        /// Pin 97.
+        /// </summary>
+        Pin97 = 97,
+
+        /// <summary>
+        /// Pin 98.
+        /// </summary>
+        Pin98 = 98,
+
+        /// <summary>
+        /// Pin 99.
+        /// </summary>
+        Pin99 = 99,
+
+        /// <summary>
+        /// Pin 100.
+        /// </summary>
+        Pin100 = 100,
+
+        /// <summary>
+        /// Pin 101.
+        /// </summary>
+        Pin101 = 101,
+
+        /// <summary>
+        /// Pin 102.
+        /// </summary>
+        Pin102 = 102,
+
+        /// <summary>
+        /// Pin 103.
+        /// </summary>
+        Pin103 = 103,
+
+        /// <summary>
+        /// Pin 104.
+        /// </summary>
+        Pin104 = 104,
+
+        /// <summary>
+        /// Pin 105.
+        /// </summary>
+        Pin105 = 105,
+
+        /// <summary>
+        /// Pin 106.
+        /// </summary>
+        Pin106 = 106,
+
+        /// <summary>
+        /// Pin 107.
+        /// </summary>
+        Pin107 = 107,
+
+        /// <summary>
+        /// Pin 108.
+        /// </summary>
+        Pin108 = 108,
+
+        /// <summary>
+        /// Pin 109.
+        /// </summary>
+        Pin109 = 109,
+
+        /// <summary>
+        /// Pin 110.
+        /// </summary>
+        Pin110 = 110,
+
+        /// <summary>
+        /// Pin 111.
+        /// </summary>
+        Pin111 = 111,
+
+        /// <summary>
+        /// Pin 112.
+        /// </summary>
+        Pin112 = 112,
+
+        /// <summary>
+        /// Pin 113.
+        /// </summary>
+        Pin113 = 113,
+
+        /// <summary>
+        /// Pin 114.
+        /// </summary>
+        Pin114 = 114,
+
+        /// <summary>
+        /// Pin 115.
+        /// </summary>
+        Pin115 = 115,
+
+        /// <summary>
+        /// Pin 116.
+        /// </summary>
+        Pin116 = 116,
+
+        /// <summary>
+        /// Pin 117.
+        /// </summary>
+        Pin117 = 117,
+
+        /// <summary>
+        /// Pin 118.
+        /// </summary>
+        Pin118 = 118,
+
+        /// <summary>
+        /// Pin 119.
+        /// </summary>
+        Pin119 = 119,
+
+        /// <summary>
+        /// Pin 120.
+        /// </summary>
+        Pin120 = 120,
+
+        /// <summary>
+        /// Pin 121.
+        /// </summary>
+        Pin121 = 121,
+
+        /// <summary>
+        /// Pin 122.
+        /// </summary>
+        Pin122 = 122,
+
+        /// <summary>
+        /// Pin 123.
+        /// </summary>
+        Pin123 = 123,
+
+        /// <summary>
+        /// Pin 124.
+        /// </summary>
+        Pin124 = 124,
+
+        /// <summary>
+        /// Pin 125.
+        /// </summary>
+        Pin125 = 125,
+
+        /// <summary>
+        /// Pin 126.
+        /// </summary>
+        Pin126 = 126,
+
+        /// <summary>
+        /// Pin 127.
+        /// </summary>
+        Pin127 = 127,
+
+        /// <summary>
+        /// Pin 128.
+        /// </summary>
+        Pin128 = 128,
     }
 }
+// Python Pin generator:
+//for i in range(0, 129):
+//    print('        /// <summary>')
+//    print('        /// Pin ', i, '.', sep = '')
+//    print('        /// </summary>')
+//    print('        Pin',i, ' = ', i, ',\n', sep = '')
